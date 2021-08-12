f3cb31c9343289ee1229c4e06f4c22daadcc15f8
王阳 <wangyang@sensorsdata.cn>
2021-08-12 14:44:16 +0800

    修改，使用脚本打开目录。

--- search.py
+++ search.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # encoding: utf-8
 
-import sys
 import os
-import glob
+import re
+import sys
 import argparse
 
 from workflow import Workflow3
 from os.path import expanduser
 
 # 返回 workspace 目录
-def dirs(workspace,type):
+def dirs(workspace, type):
     for files in os.listdir(workspace):
         path = workspace+files
         if type == "d" and os.path.isdir(path):
@@ -21,20 +21,24 @@ def dirs(workspace,type):
             yield files
 
 def main(wf):
-    parser = argparse.ArgumentParser(description="搜索文件或目录")
-    parser.add_argument("query", metavar="query", type=str,help="查找的文件名")
-    parser.add_argument("-workspace",dest="workspace",required=True,help="在指定的目录下搜索")
-    parser.add_argument("-type",dest="type",help="搜索类型，如果是文件参数为f 如果搜索目录参数是d")
+    parser = argparse.ArgumentParser(description="搜索文件或目录。")
+    parser.add_argument("query", metavar="query", type=str, help="查找的文件名。")
+    parser.add_argument("-app", metavar="app", type=str, help="用于打开文件的 app 名字。")
+    parser.add_argument("-workspace", dest="workspace", required=True, help="在指定的目录下搜索。")
+    parser.add_argument("-type",dest="type", help="搜索类型，如果是文件参数为f 如果搜索目录参数是 d。")
     args = parser.parse_args()
     args.workspace = expanduser(args.workspace)
+
     if args.workspace and args.workspace[-1] != '/':
         args.workspace+='/'
 
+    wf.setvar(name="app_name", value=args.app)
     for dir in dirs(args.workspace,args.type):
         # 判断 query 是否是目录名前缀
         # 如果 query 是 / 返回全部目录
-        if dir.startswith(args.query.strip('/')) or args.query == "/":
-            wf.add_item(title=dir,arg=args.workspace+dir,valid=True)
+        if re.search(args.query.strip('/'), dir) is not None:
+            wf.add_item(title=dir, arg=args.workspace + dir, valid=True)
+
     wf.send_feedback()
 
 if __name__ == '__main__':
